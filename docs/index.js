90e60a1e79b0bf489672706dd33b977fc9300c79
acx29 <mikedornic@gmail.com>
2026-03-06 05:43:21 -0500

    figured out supabase client
    title

--- docs/index.js
+++ docs/index.js
@@ -2,6 +2,10 @@ let username;
 let password;
 let repeatPassword;
 
+import { createClient } from '@supabase/supabase-js'; // ok we added the url and key from my supabase project below
+const supabase = createClient('https://cseushjckimnurbnuxcy.supabase.co', 'sb_publishable_Xj0uKhbrEbdSJSkIynNXpg_xD9hntDu');
+
+
 document.getElementById("mySubmit").onclick = function(){
     username = document.getElementById("username").value;
     password = document.getElementById("password").value;
