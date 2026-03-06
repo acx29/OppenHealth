6dfd02547bf080e8e6074ec8f403cc867941d5a7
acx29 <mikedornic@gmail.com>
2026-03-06 05:30:08 -0500

    added stuff imported supabase
    idk how to do the supabase auth tho

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,14 @@
 let username;
+let password;
+let repeatPassword;
 
 document.getElementById("mySubmit").onclick = function(){
     username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+    repeatPassword = document.getElementById("repeat-password").value;
+
     console.log(username);
+    console.log(password);
+    console.log(repeatPassword); 
 }
+
