4a9eaf5f0e2e5eb1d28932f71c02062c38bebf2c
acx29 <mikedornic@gmail.com>
2026-03-19 04:54:32 -0400

    troubleshooting dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,5 +1,7 @@
 // logic for redirecting user on dashboard html file
 
+console.log("dashboard.js loaded"); // j to check if this loads
+
 async function logoutUser(){
     await fetch("/api/logout", { // calling the logout route in authRoutes.js
         method: "POST",
